a0b582abe90f294dee87fab0914be64613a80a5c
Shubham Naik Ramavatu <shubham.ramavatu@capgemini.com>
2018-04-06 15:25:52 +0530

    first commit

---
+++ src/main/java/Hello_World/Hello_World/App.java
@@ -0,0 +1,13 @@
+package Hello_World.Hello_World;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
